f33e7c6f0bc9d07599c3a1c3ab9dc01ec5b90055
Vamsi <pvsvamsi2009@gmail.com>
2020-01-30 15:33:42 +0530

    Updated the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
     super.initState();
     _initPlatformState();
     _checkPermissions();
-    _registerCallBack();
+    SystemAlertWindow.registerOnClickListener(_callBack);
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -47,14 +47,8 @@ class _MyAppState extends State<MyApp> {
     await SystemAlertWindow.checkPermissions;
   }
 
-  void _registerCallBack() {
-    SystemAlertWindow.registerCallBack((dynamic arguments) {
-      String type = arguments[0];
-      if (type == "onClick") {
-        String tag = arguments[1];
-        print("OnClick event of $tag");
-      }
-    });
+  void _callBack(String tag){
+    print("OnClick event of $tag");
   }
 
   void _showOverlayWindow() {
@@ -124,68 +118,6 @@ class _MyAppState extends State<MyApp> {
           gravity: SystemWindowGravity.TOP);
       _isShowingWindow = true;
     } else {
-      /*SystemWindowHeader header = SystemWindowHeader(
-        title: SystemWindowText(text: "Incoming Call", fontSize: 10, textColor: Colors.black45),
-        padding: SystemWindowPadding.setSymmetricPadding(12, 12),
-        subTitle: SystemWindowText(text: "9898989899", fontSize: 14, fontWeight: FontWeight.BOLD, textColor: Colors.black87),
-        decoration: SystemWindowDecoration(startColor: Colors.grey[100]),
-      );
-      SystemWindowBody body = SystemWindowBody(
-        rows: [
-          EachRow(
-            columns: [
-              EachColumn(
-                text: SystemWindowText(text: "Some body", fontSize: 12, textColor: Colors.black45),
-              ),
-            ],
-            gravity: ContentGravity.CENTER,
-          ),
-          EachRow(columns: [
-            EachColumn(
-                text: SystemWindowText(text: "Long data of the body", fontSize: 12, textColor: Colors.black87, fontWeight: FontWeight.BOLD),
-                padding: SystemWindowPadding.setSymmetricPadding(6, 8),
-                decoration: SystemWindowDecoration(startColor: Colors.black12, borderRadius: 25.0),
-                margin: SystemWindowMargin(top: 4)),
-          ], gravity: ContentGravity.CENTER),
-          EachRow(
-            columns: [
-              EachColumn(
-                text: SystemWindowText(text: "Notes", fontSize: 10, textColor: Colors.black45),
-              ),
-            ],
-            gravity: ContentGravity.LEFT,
-            margin: SystemWindowMargin(top: 8),
-          ),
-          EachRow(
-            columns: [
-              EachColumn(
-                text: SystemWindowText(text: "Some random notes.", fontSize: 13, textColor: Colors.black54, fontWeight: FontWeight.BOLD),
-              ),
-            ],
-            gravity: ContentGravity.LEFT,
-          ),
-        ],
-        padding: SystemWindowPadding(left: 16, right: 16, bottom: 12, top: 12),
-      );
-      SystemWindowFooter footer = SystemWindowFooter(
-          button: SystemWindowButton(
-            text: SystemWindowText(text: "I'm a button", fontSize: 12, textColor: Colors.white),
-            tag: "footer_button",
-            width: SystemWindowButton.MATCH_PARENT,
-            height: SystemWindowButton.WRAP_CONTENT,
-            decoration: SystemWindowDecoration(
-                startColor: Color.fromRGBO(250, 139, 97, 1), endColor: Color.fromRGBO(247, 28, 88, 1), borderWidth: 0, borderRadius: 30.0),
-          ),
-          padding: SystemWindowPadding(left: 16, right: 16, bottom: 12),
-          decoration: SystemWindowDecoration(startColor: Colors.white),
-          buttonPosition: ButtonPosition.CENTER);
-      SystemAlertWindow.showSystemWindow(
-          height: 230,
-          header: header,
-          body: body,
-          footer: footer,
-          margin: SystemWindowMargin(left: 8, right: 8, top: 200, bottom: 0),
-          gravity: SystemWindowGravity.TOP);*/
       SystemAlertWindow.closeSystemWindow();
       _isShowingWindow = false;
     }
@@ -196,7 +128,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('System Alert Window Example App'),
         ),
         body: Center(
           child: Column(
@@ -207,7 +139,7 @@ class _MyAppState extends State<MyApp> {
                 child: MaterialButton(
                   onPressed: _showOverlayWindow,
                   textColor: Colors.white,
-                  child: Text("Show overlay"),
+                  child: Text("Show system alert window"),
                   color: Colors.deepOrange,
                   padding: const EdgeInsets.symmetric(vertical: 8.0),
                 ),
