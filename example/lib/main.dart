22e8ea232ce018166077c932d34848c4c7fff18f
Vamsi <pvsvamsi2009@gmail.com>
2020-04-08 00:36:23 +0530

    Updated the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   bool _isShowingWindow = false;
+  bool _isUpdatedWindow = false;
 
   @override
   void initState() {
@@ -101,8 +102,7 @@ class _MyAppState extends State<MyApp> {
               padding: SystemWindowPadding(left: 10, right: 10, bottom: 10, top: 10),
               width: 0,
               height: SystemWindowButton.WRAP_CONTENT,
-              decoration: SystemWindowDecoration(
-                  startColor: Colors.white, endColor: Colors.white, borderWidth: 0, borderRadius: 0.0),
+              decoration: SystemWindowDecoration(startColor: Colors.white, endColor: Colors.white, borderWidth: 0, borderRadius: 0.0),
             ),
             SystemWindowButton(
               text: SystemWindowText(text: "Focus button", fontSize: 12, textColor: Colors.white),
@@ -124,10 +124,93 @@ class _MyAppState extends State<MyApp> {
           footer: footer,
           margin: SystemWindowMargin(left: 8, right: 8, top: 200, bottom: 0),
           gravity: SystemWindowGravity.TOP);
-      _isShowingWindow = true;
+      setState(() {
+        _isShowingWindow = true;
+      });
+    } else if (!_isUpdatedWindow) {
+      SystemWindowHeader header = SystemWindowHeader(
+          title: SystemWindowText(text: "Outgoing Call", fontSize: 10, textColor: Colors.black45),
+          padding: SystemWindowPadding.setSymmetricPadding(12, 12),
+          subTitle: SystemWindowText(text: "8989898989", fontSize: 14, fontWeight: FontWeight.BOLD, textColor: Colors.black87),
+          decoration: SystemWindowDecoration(startColor: Colors.grey[100]),
+          button: SystemWindowButton(text: SystemWindowText(text: "Personal", fontSize: 10, textColor: Colors.black45), tag: "personal_btn"),
+          buttonPosition: ButtonPosition.TRAILING);
+      SystemWindowBody body = SystemWindowBody(
+        rows: [
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Updated body", fontSize: 12, textColor: Colors.black45),
+              ),
+            ],
+            gravity: ContentGravity.CENTER,
+          ),
+          EachRow(columns: [
+            EachColumn(
+                text: SystemWindowText(text: "Updated long data of the body", fontSize: 12, textColor: Colors.black87, fontWeight: FontWeight.BOLD),
+                padding: SystemWindowPadding.setSymmetricPadding(6, 8),
+                decoration: SystemWindowDecoration(startColor: Colors.black12, borderRadius: 25.0),
+                margin: SystemWindowMargin(top: 4)),
+          ], gravity: ContentGravity.CENTER),
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Notes", fontSize: 10, textColor: Colors.black45),
+              ),
+            ],
+            gravity: ContentGravity.LEFT,
+            margin: SystemWindowMargin(top: 8),
+          ),
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Updated random notes.", fontSize: 13, textColor: Colors.black54, fontWeight: FontWeight.BOLD),
+              ),
+            ],
+            gravity: ContentGravity.LEFT,
+          ),
+        ],
+        padding: SystemWindowPadding(left: 16, right: 16, bottom: 12, top: 12),
+      );
+      SystemWindowFooter footer = SystemWindowFooter(
+          buttons: [
+            SystemWindowButton(
+              text: SystemWindowText(text: "Updated Simple button", fontSize: 12, textColor: Color.fromRGBO(250, 139, 97, 1)),
+              tag: "updated_simple_button",
+              padding: SystemWindowPadding(left: 10, right: 10, bottom: 10, top: 10),
+              width: 0,
+              height: SystemWindowButton.WRAP_CONTENT,
+              decoration: SystemWindowDecoration(startColor: Colors.white, endColor: Colors.white, borderWidth: 0, borderRadius: 0.0),
+            ),
+            SystemWindowButton(
+              text: SystemWindowText(text: "Focus button", fontSize: 12, textColor: Colors.white),
+              tag: "focus_button",
+              width: 0,
+              padding: SystemWindowPadding(left: 10, right: 10, bottom: 10, top: 10),
+              height: SystemWindowButton.WRAP_CONTENT,
+              decoration: SystemWindowDecoration(
+                  startColor: Color.fromRGBO(250, 139, 97, 1), endColor: Color.fromRGBO(247, 28, 88, 1), borderWidth: 0, borderRadius: 30.0),
+            )
+          ],
+          padding: SystemWindowPadding(left: 16, right: 16, bottom: 12),
+          decoration: SystemWindowDecoration(startColor: Colors.white),
+          buttonsPosition: ButtonPosition.CENTER);
+      SystemAlertWindow.updateSystemWindow(
+          height: 230,
+          header: header,
+          body: body,
+          footer: footer,
+          margin: SystemWindowMargin(left: 8, right: 8, top: 200, bottom: 0),
+          gravity: SystemWindowGravity.TOP);
+      setState(() {
+        _isUpdatedWindow = true;
+      });
     } else {
+      setState(() {
+        _isShowingWindow = false;
+        _isUpdatedWindow = false;
+      });
       SystemAlertWindow.closeSystemWindow();
-      _isShowingWindow = false;
     }
   }
 
@@ -147,7 +230,9 @@ class _MyAppState extends State<MyApp> {
                 child: MaterialButton(
                   onPressed: _showOverlayWindow,
                   textColor: Colors.white,
-                  child: Text("Show system alert window"),
+                  child: !_isShowingWindow
+                      ? Text("Show system alert window")
+                      : !_isUpdatedWindow ? Text("Update system alert window") : Text("Close system alert window"),
                   color: Colors.deepOrange,
                   padding: const EdgeInsets.symmetric(vertical: 8.0),
                 ),
@@ -165,8 +250,10 @@ class _MyAppState extends State<MyApp> {
 /// You can check for the tag value and perform the relevant action for the button click
 ///
 void callBack(String tag) {
-  switch(tag){
+  print(tag);
+  switch (tag) {
     case "simple_button":
+    case "updated_simple_button":
       SystemAlertWindow.closeSystemWindow();
       break;
     case "focus_button":
